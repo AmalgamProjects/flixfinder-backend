e3baec0765021ccb2294c35e8d89402fc5e7f0fe
Brian Wojtczak <astrolox@gmail.com>
2020-05-13 12:28:25 +0100

    PEP8 Auto format API models

--- src/ff_api/models.py
+++ src/ff_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class SeparatedValuesField(models.TextField):
 
     def __init__(self, *args, **kwargs):
@@ -14,16 +15,17 @@ class SeparatedValuesField(models.TextField):
 
     def get_db_prep_value(self, value, connection, prepared=False):
         if not value: return
-        assert(isinstance(value, list) or isinstance(value, tuple))
+        assert (isinstance(value, list) or isinstance(value, tuple))
         return self.token.join([unicode(s) for s in value])
 
     def value_to_string(self, obj):
         value = self._get_val_from_obj(obj)
         return self.get_db_prep_value(value)
-    
+
     def from_db_value(self, value, expression, connection, context):
         value = self.to_python(value)
         return value if value is not None else ''
+
 
 # Create your models here.
 class Title(models.Model):
@@ -36,7 +38,7 @@ class Title(models.Model):
     endYear = models.CharField(null=True, max_length=4)
     runtimeMinutes = models.CharField(max_length=10)
     genres = SeparatedValuesField()
-    
+
 
 class Name(models.Model):
     nconst = models.CharField(primary_key=True, max_length=255)
@@ -45,21 +47,21 @@ class Name(models.Model):
     deathYear = models.IntegerField(null=True)
     primaryProfession = SeparatedValuesField()
     knownForTitles = SeparatedValuesField()
-    
-    
+
+
 class Crew(models.Model):
     tconst = models.ForeignKey(Title, on_delete=models.CASCADE)
     directors = SeparatedValuesField()
     writers = SeparatedValuesField()
-    
+
 
 class Episode(models.Model):
     tconst = models.ForeignKey(Title, on_delete=models.CASCADE)
     parentTconst = models.CharField(max_length=255)
     seasonNumber = models.IntegerField(null=True)
     episodeNumber = models.IntegerField(null=True)
-    
-    
+
+
 class Principal(models.Model):
     tconst = models.ForeignKey(Title, on_delete=models.CASCADE)
     ordering = models.IntegerField()
@@ -67,12 +69,9 @@ class Principal(models.Model):
     category = models.CharField(max_length=255)
     job = models.CharField(max_length=255, null=True)
     characters = models.TextField(null=True)
-    
+
 
 class Rating(models.Model):
     tconst = models.ForeignKey(Title, on_delete=models.CASCADE)
     averageRating = models.DecimalField(decimal_places=2, max_digits=10)
     numVotes = models.IntegerField()
-    
-
-  
