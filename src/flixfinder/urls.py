baf0713a32bbcc8be6d9b4898df5332d31b420d2
Brian Wojtczak <astrolox@gmail.com>
2020-05-14 13:03:28 +0100

    Serve of static files even when not in DEBUG

--- src/flixfinder/urls.py
+++ src/flixfinder/urls.py
@@ -4,11 +4,15 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 
 """
+
 from django.contrib import admin
+from django.contrib.staticfiles.views import serve
 from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ff_api.urls')),
-    re_path(r'^(?!admin|api)', include('ff_spa.urls'))
+    path('api/', include('ff_api.urls')),
+    re_path(r'^(?!admin|api|static)', include('ff_spa.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'insecure': True}),
 ]
