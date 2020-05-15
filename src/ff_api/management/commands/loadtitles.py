4f838096c91717020565fb968f3ec0cc098be01c
Brian Wojtczak <astrolox@gmail.com>
2020-05-15 11:14:53 +0100

    Improved the load titles script

--- src/ff_api/management/commands/loadtitles.py
+++ src/ff_api/management/commands/loadtitles.py
@@ -1,33 +1,83 @@
 import csv
+import pprint
+import time
+
 from django.core.management import BaseCommand
-from ff_api.models import Title, Genre
+
+from ...models import Title, Genre
+
 
 class Command(BaseCommand):
     help = 'Load files into the db'
-    
+
     def add_arguments(self, parser):
-        parser.add_argument('--path', type=str)
-        
+        parser.add_argument('--path', type=str, required=True)
+        parser.add_argument('--rowstart', type=int, default=0)
+        parser.add_argument('--rowstop', type=int, default=6802944)
+
+    # noinspection PyBroadException
     def handle(self, *args, **kwargs):
-        count = 1
+        ignore_rows_before = kwargs['rowstart']
+        ignore_rows_after = kwargs['rowstop']
+        number_rows = ignore_rows_after - ignore_rows_before
+        start_time = int(time.time())
+        last_time = start_time
+        last_count = 0
+        last_dbcount = 0
+        count = 0
+        dbcount = 0
         path = kwargs['path']
+        pprint.pprint("reading " + path)
         with open(path, 'rt') as f:
             reader = csv.reader(f, delimiter="\t", quotechar='"')
             for row in reader:
+                count += 1
+                if count <= ignore_rows_before:
+                    continue
+                if count >= ignore_rows_after:
+                    break
+                now = int(time.time())
+                if last_time < now:
+                    eta = int((number_rows - (count - ignore_rows_before)) / ((count - ignore_rows_before) / (now - start_time)))
+                    if eta > 3600:
+                        eta = str(int(eta / 3600)) + ' hours'
+                    elif eta > 60:
+                        eta = str(int(eta / 60)) + ' minutes'
+                    pprint.pprint(
+                        'second: %s rows (count=%s) .. %s db (count=%s) .. eta %s' % (
+                            count - last_count, count,
+                            dbcount - last_dbcount, dbcount,
+                            eta
+                        )
+                    )
+                    last_time = now
+                    last_count = count
+                    last_dbcount = dbcount
                 try:
+                    titleType = row[1]
                     release = int(row[6])
                     running = int(row[7])
-                except:
+                except Exception:
                     continue
-                if count > 1 and release > 1990 and running > 15:
+                # movie,short,titleType,tvEpisode,tvMiniSeries,tvMovie,tvSeries,tvShort,tvSpecial,video,videoGame
+                if titleType not in ['movie', 'tvMovie', 'tvSeries', 'tvMiniSeries']:
+                    continue
+                if running < 15:
+                    continue
+                if titleType != 'movie' and release < 2000:
+                    continue
+                if titleType == 'movie' and release < 1980:
+                    continue
+                try:
                     genres = []
                     for genre in row[8].split(','):
                         try:
-                            result = Genre.objects.get(name=genre)
-                        except:
+                            result = Genre.objects.filter(name=genre).first()
+                        except Exception:
                             result = Genre.objects.create(name=genre)
                         genres.append(result)
-                    
+
+                    dbcount += 1
                     title = Title.objects.create(
                         tconst=row[0],
                         titleType=row[1],
@@ -38,7 +88,12 @@ class Command(BaseCommand):
                         endYear=row[6],
                         runtimeMinutes=row[7]
                     )
-                    
                     title.save()
                     title.genres.set(genres)
-                count += 1
+                    pprint.pprint("%s - Created   %12s %8s %s (%s)" % (count, row[0], row[1], row[2], release))
+
+                except Exception as e:
+                    if 'Duplicate entry' not in str(e):
+                        pprint.pprint("%s - Exception %12s %8s %s (%s)" % (count, row[0], row[1], row[2], release))
+                        pprint.pprint(str(e))
+                    pass
