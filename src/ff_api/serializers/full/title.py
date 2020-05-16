869a587433ec1ea981e5d89d2c16136e8e7a0431
Brian Wojtczak <astrolox@gmail.com>
2020-05-16 15:21:45 +0100

    Improved output of actor/principal data

--- src/ff_api/serializers/full/title.py
+++ src/ff_api/serializers/full/title.py
@@ -3,6 +3,9 @@ http://www.django-rest-framework.org/api-guide/serializers/
 
 """
 
+import json
+
+from codecs import encode, decode
 from rest_framework import serializers
 
 from .imdb import PrincipalSerializer, NameSerializer
@@ -64,13 +67,27 @@ class TitleSerializer(serializers.HyperlinkedModelSerializer):
             # 'rapid',
         ]
 
+    def _no_fake_null(self, string):
+        if string == '\\N':
+            return None
+        return string
+
+    def _fixup_characters(self, string):
+        list = self._no_fake_null(string)
+        if list is not None:
+            list = decode(encode(list, 'latin-1', 'backslashreplace'), 'unicode-escape')
+            list = json.loads(list)
+        return list
+
     def get_principals(self, instance):
         result = []
         for principal in instance.principals.all():
-            # principal_data = PrincipalSerializer(principal, context=self.context).data
+            principal_data = PrincipalSerializer(principal, context=self.context).data
             name_data = NameSerializer(principal.nconst, context=self.context).data
-            # name_data['job'] = principal_data['job']
-            # name_data['category'] = principal_data['category']
+            name_data['job'] = self._no_fake_null(principal_data['job'])
+            name_data['category'] = self._no_fake_null(principal_data['category'])
+            name_data['characters'] = self._fixup_characters(principal_data['characters'])
+            name_data['image_url'] = None
             result.append(name_data)
         return result
 
