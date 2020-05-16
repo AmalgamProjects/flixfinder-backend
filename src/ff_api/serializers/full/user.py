46600f44d36dc0c9c70ef3137cccbb0ce56c7d1f
Brian Wojtczak <astrolox@gmail.com>
2020-05-16 01:33:44 +0100

    Added separate display of movie and tv recommendations

--- src/ff_api/serializers/full/user.py
+++ src/ff_api/serializers/full/user.py
@@ -22,7 +22,11 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     favouriteGenres = ShallowFavouriteGenreSerializer(many=True, read_only=True, source='favourite_genres')
     seen = ShallowSeenSerializer(many=True, read_only=True)
     watch = ShallowWatchSerializer(many=True, read_only=True)
+
     recommended = ShallowRecommendationSerializer(many=True, read_only=True, source='recommendation')
+
+    recommended_movies = serializers.SerializerMethodField()
+    recommended_tv = serializers.SerializerMethodField()
 
     class Meta:
         model = User
@@ -34,4 +38,22 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
             'seen',
             'watch',
             'recommended',
+            'recommended_movies',
+            'recommended_tv',
         ]
+
+    def get_recommended_movies(self, instance):
+        result = []
+        for recommendation in instance.recommendation.get_queryset().filter(title__titleType='movie'):
+            if recommendation.title.titleType == 'movie':
+                serializer = ShallowRecommendationSerializer(recommendation, context=self.context)
+                result.append(serializer.data)
+        return result
+
+    def get_recommended_tv(self, instance):
+        result = []
+        for recommendation in instance.recommendation.get_queryset().exclude(title__titleType='movie'):
+            if recommendation.title.titleType[:2] == 'tv':
+                serializer = ShallowRecommendationSerializer(recommendation, context=self.context)
+                result.append(serializer.data)
+        return result
